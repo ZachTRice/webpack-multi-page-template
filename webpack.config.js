d0d6dd1970919243e7354837a8a3010d3b4ff994
ZachTRice <zachtrice@gmail.com>
2018-05-16 10:49:38 -0400

    Add stylelint, eslint & test commands. Add github docs & license

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const devMode = process.env.NODE_ENV !== 'production'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const devMode = process.env.NODE_ENV !== 'production';
 
 function recursiveIssuer(m) {
   if (m.issuer) {
@@ -16,7 +16,7 @@ function recursiveIssuer(m) {
 module.exports = {
   entry: {
     'index': './src/js/index.js',
-    'subpage': './src/js/subpage/index.js',
+    'subpage': './src/js/subpage/index.js'
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -24,20 +24,20 @@ module.exports = {
   },
   output: {
     filename: '[name].js',
-    path: __dirname + '/dist'
+    path: path.join(__dirname, '/dist')
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         indexStyles: {
           name: 'index',
-          test: (m,c,entry = 'index') => m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
+          test: (m, c, entry = 'index') => m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
           chunks: 'all',
           enforce: true
         },
         subpageStyles: {
           name: 'subpage',
-          test: (m,c,entry = 'subpage') => m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
+          test: (m, c, entry = 'subpage') => m.constructor.name === 'CssModule' && recursiveIssuer(m) === entry,
           chunks: 'all',
           enforce: true
         }
@@ -46,24 +46,24 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        hash: true,
-        title: 'Index',
-        template: './src/pages/index.html',
-        filename: './index.html',
-        inject: false
+      hash: true,
+      title: 'Index',
+      template: './src/pages/index.html',
+      filename: './index.html',
+      inject: false
     }),
     new HtmlWebpackPlugin({
-        hash: true,
-        title: 'Subpage',
-        template: './src/pages/subpage/index.html',
-        filename: './subpage.html',
-        inject: false
+      hash: true,
+      title: 'Subpage',
+      template: './src/pages/subpage/index.html',
+      filename: './subpage.html',
+      inject: false
     }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: '[name].css',
-      chunkFilename: '[id].css',
+      chunkFilename: '[id].css'
     })
   ],
   module: {
@@ -72,7 +72,7 @@ module.exports = {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader"
+          'css-loader'
         ]
       },
       {
@@ -95,8 +95,8 @@ module.exports = {
         ]
       },
       {
-       test: /\.html$/,
-       loader: "raw-loader"
+        test: /\.html$/,
+        loader: 'raw-loader'
       }
     ]
   }
